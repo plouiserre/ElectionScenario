9fc6de07228fbab70965815fdbd23fddf08c46d3
plouiserre <plouiserre@gmail.com>
2025-10-31 13:53:31 +0100

    first usecase ok

--- src/backend/domain/services/OneTurnElection/BuildCongress.py
+++ src/backend/domain/services/OneTurnElection/BuildCongress.py
@@ -5,8 +5,36 @@ class BuildCongress :
        pass
 
     def Build(self, year, mode, parties): 
+        congress = self.__build_congress(year, mode, parties)
+        is_percentage_correct = self.__check_and_correct_parties_percentages(congress)
+        if is_percentage_correct == False: 
+            congress = self.__remove_percentage_to_have_one_hundred_percentage(congress)
+        return congress
+
+    def __build_congress(self, year, mode, parties):
         congress = Congress()
         congress.year = year 
         congress.mode = mode 
         congress.parties = parties
+        return congress
+    
+    def __check_and_correct_parties_percentages(self, congress):
+        percentage = 0.0
+        for party in congress.parties : 
+            percentage += party.percentage
+        if percentage == 100 :
+            return True
+        else :
+            return False
+        
+    def __remove_percentage_to_have_one_hundred_percentage(self, congress): 
+        low_percentage = 100
+        party_code_with_low_percentage = ''
+        for party in congress.parties : 
+            if party.percentage < low_percentage:
+                low_percentage = party.percentage 
+                party_code_with_low_percentage = party.code
+        for party in congress.parties : 
+            if party.code == party_code_with_low_percentage : 
+                party.percentage = party.percentage -  0.01
         return congress
--- tests/OneTurnElectionTests/test_build_congress.py
+++ tests/OneTurnElectionTests/test_build_congress.py
@@ -17,4 +17,20 @@ class BuildCongressTest(unittest.TestCase):
             self.assertEqual(parties_2024[idx].name, parties_expected[idx].name)
             self.assertEqual(parties_2024[idx].percentage, parties_expected[idx].percentage)
             self.assertEqual(len(parties_2024[idx].congress_persons), len(parties_expected[idx].congress_persons))
+
+
+    def test_build_congress_with_no_one_hundred_by_zero_point_one_percent(self):
+        parties_2024 = get_parties_with_elected_persons_2024(42.86, 14.29, 42.86)
+        build_congress = BuildCongress()
+
+        congress = build_congress.Build(2024, "OneTurn", parties_2024)
+
+        parties_expected = get_parties_with_elected_persons_2024(42.86, 14.28, 42.86)
+        self.assertEqual(2024, congress.year)
+        self.assertEqual("OneTurn", congress.mode)
+        for idx in range(len(parties_expected)):
+            self.assertEqual(parties_2024[idx].code, parties_expected[idx].code)
+            self.assertEqual(parties_2024[idx].name, parties_expected[idx].name)
+            self.assertEqual(parties_2024[idx].percentage, parties_expected[idx].percentage)
+            self.assertEqual(len(parties_2024[idx].congress_persons), len(parties_expected[idx].congress_persons))
         
