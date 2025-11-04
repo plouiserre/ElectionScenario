f06a77773a70949b4f400f0e1921c88034c7efd9
plouiserre <plouiserre@gmail.com>
2025-11-04 11:03:40 +0100

    infra services worked

---
+++ src/backend/domain/models/elections.py
@@ -0,0 +1,4 @@
+class Elections: 
+    def __init__(self):
+        self.all_candidates = []
+        self.all_parties = []
--- src/backend/domain/models/factory.py
+++ src/backend/domain/models/factory.py
@@ -2,6 +2,7 @@
 from src.backend.domain.models.congressPerson import CongressPerson
 from src.backend.domain.models.congress import Congress
 from src.backend.domain.models.district import District
+from src.backend.domain.models.elections import Elections
 from src.backend.domain.models.party import Party
 
 def factory_district(name, code, department_name, department_code):
@@ -22,6 +23,12 @@ def factory_congress_person(last_name, first_name, sexe, parti_code, vote, vote_
     congress_person.vote_percentage = vote_percentage
     congress_person.district = district
     return congress_person
+
+def factory_elections(candidates, parties):
+    elections = Elections()
+    elections.all_candidates = candidates
+    elections.all_parties = parties 
+    return elections
 
 def factory_party(name, code, percentage, congress_persons): 
     party = Party()
