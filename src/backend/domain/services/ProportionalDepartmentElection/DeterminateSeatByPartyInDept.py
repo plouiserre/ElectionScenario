30adbb0cd89d8e1848964feb0bbfbc8ea09422ec
plouiserre <plouiserre@gmail.com>
2026-01-02 14:57:27 +0100

    continue development

--- src/backend/domain/services/ProportionalDepartmentElection/DeterminateSeatByPartyInDept.py
+++ src/backend/domain/services/ProportionalDepartmentElection/DeterminateSeatByPartyInDept.py
@@ -3,6 +3,31 @@ class DeterminateSeatsByPartyInDept :
         pass
 
     def Determinate(self, parties_votes, mode_design, number_congress_persons):
-        seats_party = {}
-        seats_party["UXD"] = 1
-        return seats_party
+        parties_votes_ordered_by_scores = self.__ordered_by_scores(parties_votes)
+        parties_votes_selected = self.__keep_selected_parties(parties_votes_ordered_by_scores, number_congress_persons)
+        seats_by_parties = self.__determinate_each_party_candidates_elected(parties_votes_selected)
+        return seats_by_parties
+    
+    def __ordered_by_scores(self, parties_votes): 
+        parties_votes_ordered_by_scores = sorted(parties_votes.items(),  key=lambda item: item[1], reverse=True)
+        return parties_votes_ordered_by_scores
+    
+    def __keep_selected_parties(self, parties_vote, number_congress_persons) : 
+        parties_votes_selected = {}
+        i = 0
+        for party in parties_vote : 
+            if i < number_congress_persons :
+                parties_votes_selected[party[0]] = party[1]
+                i += 1
+            else :
+                break
+        return parties_votes_selected
+    
+    def __determinate_each_party_candidates_elected(self, parties_votes_selected): 
+        results = {}
+        for party in parties_votes_selected : 
+            if party in results : 
+                results[party] += 1
+            else : 
+                results[party] = 1
+        return results
--- tests/ProportionalDepartmentElectionTests/test_determinate_seat_by_party_in_dept.py
+++ tests/ProportionalDepartmentElectionTests/test_determinate_seat_by_party_in_dept.py
@@ -12,3 +12,16 @@ class DeterminateSeatByPartyInDeptTest(unittest.TestCase):
         
         self.assertEqual(1,len(seats_party))
         self.assertEqual(1,seats_party["UXD"])
+
+
+    def test_choose_three_parties_elected(self):
+        mode_design = factory_mode_design("order", 12.5)
+        determinate_seats_by_party_in_dept = DeterminateSeatsByPartyInDept()
+        parties_votes = {"DVD" : 2.16, "ENS" : 9.32, "UDI" : 0.79, "EXG" : 1.43, "LR" : 24.04, "REC":0.68, "RN" : 37.00, "UG" : 24.58}   
+
+        seats_party = determinate_seats_by_party_in_dept.Determinate(parties_votes, mode_design, 3)
+        
+        self.assertEqual(3,len(seats_party))
+        self.assertEqual(1,seats_party["RN"])
+        self.assertEqual(1,seats_party["UG"])
+        self.assertEqual(1,seats_party["LR"])
