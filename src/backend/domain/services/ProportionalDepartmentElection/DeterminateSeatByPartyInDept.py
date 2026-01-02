aeae0c3d5ee668299426b2887fdb4c0998e49929
plouiserre <plouiserre@gmail.com>
2026-01-02 15:23:18 +0100

    continue working

--- src/backend/domain/services/ProportionalDepartmentElection/DeterminateSeatByPartyInDept.py
+++ src/backend/domain/services/ProportionalDepartmentElection/DeterminateSeatByPartyInDept.py
@@ -1,10 +1,13 @@
 class DeterminateSeatsByPartyInDept : 
     def __init__(self):
-        pass
+        self.number_congress_persons = 0
+        self.mode_design = None
 
     def Determinate(self, parties_votes, mode_design, number_congress_persons):
+        self.mode_design = mode_design
+        self.number_congress_persons = number_congress_persons
         parties_votes_ordered_by_scores = self.__ordered_by_scores(parties_votes)
-        parties_votes_selected = self.__keep_selected_parties(parties_votes_ordered_by_scores, number_congress_persons)
+        parties_votes_selected = self.__keep_selected_parties(parties_votes_ordered_by_scores)
         seats_by_parties = self.__determinate_each_party_candidates_elected(parties_votes_selected)
         return seats_by_parties
     
@@ -12,11 +15,11 @@ class DeterminateSeatsByPartyInDept :
         parties_votes_ordered_by_scores = sorted(parties_votes.items(),  key=lambda item: item[1], reverse=True)
         return parties_votes_ordered_by_scores
     
-    def __keep_selected_parties(self, parties_vote, number_congress_persons) : 
+    def __keep_selected_parties(self, parties_vote) : 
         parties_votes_selected = {}
         i = 0
         for party in parties_vote : 
-            if i < number_congress_persons :
+            if i < self.number_congress_persons and self.mode_design.minimal_vote <= party[1] :
                 parties_votes_selected[party[0]] = party[1]
                 i += 1
             else :
@@ -24,10 +27,29 @@ class DeterminateSeatsByPartyInDept :
         return parties_votes_selected
     
     def __determinate_each_party_candidates_elected(self, parties_votes_selected): 
+        results = self.__give_bonus_winner(parties_votes_selected)
+        while self.number_congress_persons > 0:
+            for party in parties_votes_selected : 
+                if self.number_congress_persons > 0 :
+                    if party in results : 
+                        results[party] += 1
+                        self.number_congress_persons -= 1
+                    else : 
+                        results[party] = 1
+                        self.number_congress_persons -= 1
+                else :
+                    break
+        return results
+    
+    def __give_bonus_winner(self, parties_votes_selected):
         results = {}
-        for party in parties_votes_selected : 
-            if party in results : 
-                results[party] += 1
-            else : 
-                results[party] = 1
+        if self.mode_design.type == "winner" : 
+            is_first_iteration = True
+            for party in parties_votes_selected : 
+                if is_first_iteration : 
+                    results[party] = 1
+                    self.number_congress_persons -= 1
+                    is_first_iteration = False
+                else : 
+                    break
         return results
--- tests/ProportionalDepartmentElectionTests/test_determinate_seat_by_party_in_dept.py
+++ tests/ProportionalDepartmentElectionTests/test_determinate_seat_by_party_in_dept.py
@@ -25,3 +25,29 @@ class DeterminateSeatByPartyInDeptTest(unittest.TestCase):
         self.assertEqual(1,seats_party["RN"])
         self.assertEqual(1,seats_party["UG"])
         self.assertEqual(1,seats_party["LR"])
+
+
+    def test_choose_ten_parties_elected(self):
+        mode_design = factory_mode_design("order", 12.5)
+        determinate_seats_by_party_in_dept = DeterminateSeatsByPartyInDept()
+        parties_votes = {"DIV" : 0.12, "DVC" : 0.92, "DVD" : 0.54, "DVG" : 0.04, "ECO" : 0.37, "ENS":28.24, "EXG" : 1.23, "LR" : 2.06, "REC":0.69,
+                         "RN":32.92, "UG":32.93}   
+
+        seats_party = determinate_seats_by_party_in_dept.Determinate(parties_votes, mode_design, 12)
+        
+        self.assertEqual(3,len(seats_party))
+        self.assertEqual(4,seats_party["UG"])
+        self.assertEqual(4,seats_party["RN"])
+        self.assertEqual(4,seats_party["ENS"])
+
+    
+    def test_choose_three_parties_elected_with_winner_bonus(self):
+        mode_design = factory_mode_design("winner", 12.5)
+        determinate_seats_by_party_in_dept = DeterminateSeatsByPartyInDept()
+        parties_votes = {"DVD" : 2.16, "ENS" : 9.32, "UDI" : 0.79, "EXG" : 1.43, "LR" : 14.04, "REC":0.68, "RN" : 17.00, "UG" : 54.58}   
+
+        seats_party = determinate_seats_by_party_in_dept.Determinate(parties_votes, mode_design, 3)
+        
+        self.assertEqual(2,len(seats_party))
+        self.assertEqual(2,seats_party["UG"])
+        self.assertEqual(1,seats_party["RN"])
