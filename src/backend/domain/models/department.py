adac2c3c118659c1ad08201a671a07afcbf1af11
plouiserre <plouiserre@gmail.com>
2026-01-14 16:33:51 +0100

    first version

--- src/backend/domain/models/department.py
+++ src/backend/domain/models/department.py
@@ -2,3 +2,4 @@ class Department :
     def __init__(self):
         self.code = ""
         self.name = ""
+        self.number_congress_persons = 0
---
+++ src/backend/domain/services/GlobalElection/CongressPersonsElectedEachDepartment.py
@@ -0,0 +1,20 @@
+class CongressPersonsElectedEachDepartment:
+
+    def __init__(self):
+        pass
+
+    def determinate(self, elections_results, year):
+        number_elected_persons_by_dept = {}
+        depts_completed = []
+        election = elections_results[year]
+        for candidates_in_dpt in election.all_candidates : 
+            department_code = candidates_in_dpt[0].district.department_code
+            if department_code in number_elected_persons_by_dept :
+                number_elected_persons_by_dept[department_code] += 1
+            else :
+                number_elected_persons_by_dept[department_code] = 1
+        for departement in elections_results[year].all_departments : 
+            if departement.code in number_elected_persons_by_dept : 
+                departement.number_congress_persons = number_elected_persons_by_dept[departement.code]
+                depts_completed.append(departement)
+        return depts_completed
