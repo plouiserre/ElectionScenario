c4eaed3336ad926d638aa4b848e36a457269fb40
plouiserre <plouiserre@gmail.com>
2026-01-18 12:29:51 +0100

    integrate CongressPersonsElectedForEachDepartment in ProportionalDepartmentElectionService.py

--- src/backend/domain/services/GlobalElection/CongressPersonsElectedForEachDepartment.py
+++ src/backend/domain/services/GlobalElection/CongressPersonsElectedForEachDepartment.py
@@ -3,7 +3,7 @@ class CongressPersonsElectedForEachDepartment:
     def __init__(self):
         pass
 
-    def determinate(self, elections_results, year):
+    def Determinate(self, elections_results, year):
         number_elected_persons_by_dept = {}
         depts_completed = []
         election = elections_results[year]
